ced50e22c813b4e9ba3046086beee391abc48cb4
amitkumardss012 <amitkumaryadav62029@gmail.com>
2025-05-22 12:16:44 +0530

    sjfkjslkjfldsfklds

--- src/api/routes/qr.routes.ts
+++ src/api/routes/qr.routes.ts
@@ -106,8 +106,8 @@ Qr.get(
         return next(new ErrorResponse("QR code not found", statusCode.Not_Found));
       }
       return SuccessResponse(res, "QR code fetched successfully", qrCode);
-    } catch (error: any) {
-      return next(new ErrorResponse(`Failed to fetch QR cod ${error.messsage}`, statusCode.Internal_Server_Error));
+    } catch (error) {
+      return next(new ErrorResponse(`Failed to fetch QR code`, statusCode.Internal_Server_Error));
     }
   })
 );
